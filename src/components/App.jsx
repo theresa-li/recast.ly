5ac74ca9de541773c905b4e96bff74031ad06d06
theresa-li <tli2@cca.edu>
2019-03-04 20:40:24 -0800

    Complete first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import VideoList from './VideoList.js'
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,12 +12,19 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList />
       </div>
     </div>
   </div>
 );
-
+console.log('test');
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
+/*
+babel . --out-dir compiled --presets=react --ignore=node_modules,compiled --source-maps inline
+npx babel script.js --watch --out-file script-compiled.js
+
+npx babel . --out-dir compiled
+*/
