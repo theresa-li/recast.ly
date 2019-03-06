c2afa6e79285535f3b8bf225d3d00f1c7a0e337a
theresa-li <tli2@cca.edu>
2019-03-05 17:21:57 -0800

    Instantiate VideoListEntry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,7 +3,7 @@ import VideoListEntry from './VideoListEntry.js';
 var VideoList = (props) => (
   <div className="video-list">
     {props.videos.map((video, index) => {
-      return <VideoListEntry key={index} video={video}/>
+      return <VideoListEntry key={index} video={video}/>;
     })}
   </div>
 );
